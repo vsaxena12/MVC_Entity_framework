78b51379a21efed6a70af7e7fb3f8830792a41ef
vsaxna12 <vsaxena1@binghamton.edu>
2019-11-13 02:50:15 -0600

    Store

---
+++ LeaveBased_RoleBased/LeaveBased_RoleBased/Controllers/RoleController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveBased_RoleBased.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class RoleController : Controller
+    {
+        RoleManager<IdentityRole> roleManager;
+
+        /// 
+        /// Injecting Role Manager
+        /// 
+        /// 
+        public RoleController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            var roles = roleManager.Roles.ToList();
+            return View(roles);
+        }
+
+        public IActionResult Create()
+        {
+            return View(new IdentityRole());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(IdentityRole role)
+        {
+            await roleManager.CreateAsync(role);
+            return RedirectToAction("Index");
+        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
